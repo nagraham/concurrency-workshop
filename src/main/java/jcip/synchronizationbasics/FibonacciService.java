d585926dc474ac5d95302e3bd4fc560649542772
Alex Graham <nagraham00@gmail.com>
2019-11-20 09:12:44 -0800

    Add new FibonacciService route using an Immutable cache to encapsulate the check-then-get operation

--- src/main/java/jcip/synchronizationbasics/FibonacciService.java
+++ src/main/java/jcip/synchronizationbasics/FibonacciService.java
@@ -20,7 +20,7 @@ public class FibonacciService {
 
     public static void main(String[] args) {
         logger.info("Starting my REST server!");
-        get("/fibonacci/:num", new UnsafeCachingFibonacci());
+        get("/fibonacci/:num", new ImmutableCachingFibonacci());
     }
 
     /**
@@ -72,6 +72,78 @@ public class FibonacciService {
                 lastInput.set(input);
                 cachedFibonacci.set(result);
                 return result;
+            }
+        }
+    }
+
+    /**
+     * This works -- no issues due to race conditions. And the version number appeared to increase monotonically
+     * across cache hits. Although I am sure my benchmark is too scrappy to make solid conclusions on, it is clear
+     * that on average it has better performance than the synchronized version.
+     *
+     * Runs (ms): 1105, 821, 897, 818, 784, 848, 806 (868 avg)
+     */
+    private static class ImmutableCachingFibonacci implements Route {
+        private volatile ImmutableOneFibonacciCache cache = ImmutableOneFibonacciCache.emptyCache();
+
+        @Override
+        public Object handle(Request request, Response response) throws Exception {
+            // we are assuming well formed input
+            Integer input = Integer.parseInt(request.params("num"));
+
+            // we can safely do the null check b/c the thread stores its vision of "lastFibonacci" into a local
+            // variable, which gets stashed in the thread's stack; you could also store the cache itself into a local
+            // variable, and store the result of ".update()" into the instance variable, which would make that new
+            // version visible to all threads
+            BigInteger fibonacci = cache.getLastFibonacci(input);
+            if (fibonacci == null) {
+                fibonacci = fibonacci(input);
+
+                // b/c the "cache" is volatile, when a thread calls update, it is called on the LAST written version
+                cache = cache.update(input, fibonacci);
+            } else {
+                logger.info("Cache hit! input={}, cache.version={}", input, cache.version);
+            }
+
+            return fibonacci;
+        }
+    }
+
+    /**
+     * Stores a single key/value pair, and encapsulates the logic to retrieve the value.
+     *
+     * Because this object is immutable, if it is stored in a volatile variable, the "check-then-get"
+     * compound operation is made thread safe and does not require synchronization!
+     *
+     * The immutability of the class guarantees each thread has its own copy: if thread A
+     * is performing the "check-then-get", thread B can't overwrite it and cause a race condition.
+     *
+     * Storing in a volatile variable guarantees visibility of the last written immutable cache.
+     */
+    private static class ImmutableOneFibonacciCache {
+        private final Integer lastInput;
+        private final BigInteger lastFibonacci;
+        private final int version;
+
+        private ImmutableOneFibonacciCache(Integer lastInput, BigInteger lastFibonacci, int version) {
+            this.lastInput = lastInput;
+            this.lastFibonacci = lastFibonacci;
+            this.version = version;
+        }
+
+        public static ImmutableOneFibonacciCache emptyCache() {
+            return new ImmutableOneFibonacciCache(null, null, 0);
+        }
+
+        public ImmutableOneFibonacciCache update(Integer newInput, BigInteger newFibonacci) {
+            return new ImmutableOneFibonacciCache(newInput, newFibonacci, this.version + 1);
+        }
+
+        public BigInteger getLastFibonacci(Integer input) {
+            if (lastInput == null || !lastInput.equals(input)) {
+                return null;
+            } else {
+                return lastFibonacci;
             }
         }
     }
